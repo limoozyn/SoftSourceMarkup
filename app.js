a63d8c588a4a909009496f5285f315a91290128a
Viacheslav <limoozyn@gmail.com>
2016-06-02 23:30:28 +0300

    Add company, source and modal to add companies

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-var express = require('express'),
-app = express(),
-path = require('path');
-app.use(express.static(__dirname ));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+function readyFn(){
+  var exampleInputFile = document.getElementById('exampleInputFile');
+  var exampleInputFileButton=document.getElementById('exampleInputFileButton');
+  exampleInputFileButton.onclick = function() {
+    exampleInputFile.click();
+    return false;
+}
+$('#addCompany').on('change','#exampleInputFile' , function(){ 
+      var a = document.getElementById('exampleInputFile');
+      var fileLabel = document.getElementById('fileLabel');
+      console.log(a);
+      console.log(fileLabel);
+      if(a.value == "")
+      {
+          fileLabel.innerHTML = "Файл не загружен";
+      }
+      else
+      {
+          var theSplit = a.value.split('\\');
+          fileLabel.innerHTML = theSplit[theSplit.length-1];
+      }
 });
-var server = app.listen(process.env.PORT || 3000);
+$('#addCompany .radio input').change(function(){
+  $("select").prop('disabled', true);
+  $(this).nextAll("select").prop('disabled', false);});
+}
+$( document ).ready( readyFn );
